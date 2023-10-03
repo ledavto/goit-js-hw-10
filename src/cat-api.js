6d8d5c72a4bfb9752460db9d56c41247af65ee87
ledavto <ser_ega@ukr.net>
2023-10-03 13:08:46 +0300

    import export

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,18 +1,56 @@
-function fetchBreeds() {
-  return fetch('https://api.thecatapi.com/v1/breeds').then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-}
-function renderCatList(cats) {
-  const markup = cats
-    .map(cat => {
-      return `
-          <options value =${cat.id}> ${cat.name}</options>
-        `;
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const BASE_URL = 'https://api.thecatapi.com/v1/';
+
+const instance = axios.create();
+axios.defaults.baseURL = BASE_URL;
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_q0f7O1zpFYCLwEtRAEPlPq41gEIqmJ7E4HqeSMe2H2eQkgmN85txELlTKQq8z24d';
+
+// Відправлення запросу на отримання ПОРІД котів
+function fetchBreeds(catList, elLoader) {
+  return axios
+    .get(`breeds`)
+    .then(function (response) {
+      catList.style.display = 'inline';
+      return response.data;
     })
-    .join('');
-  catList.innerHTML = markup;
+    .catch(function (error) {
+      elLoader.style.display = 'none';
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+    });
 }
+
+// function fetchBreeds() {
+//   return fetch(`${BASE_URL}breeds`).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
+
+// Очікує ідентифікатор породи, робить HTTP-запит і повертає проміс із даними про кота
+function fetchCatByBreed(id, catList, elLoader) {
+  return axios
+    .get(`images/search?breed_ids=${id}`)
+    .then(function (response) {
+      //console.log(response.data);
+      elInfo.style.display = 'block';
+      return response.data;
+    })
+    .catch(function (error) {
+      //elError.textContent = error.response.data;
+      elLoader.style.display = 'none';
+      //elError.style.display = 'inline';
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+    });
+}
+
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,65 +1,30 @@
-//import fetchBreeds from "/src/cat-api.js";
-import axios from 'axios';
+import { fetchBreeds, fetchCatByBreed } from '/src/cat-api.js';
+
+//import axios from 'axios';
 import SlimSelect from 'slim-select';
-import Notiflix from 'notiflix';
 
 // new SlimSelect({
 //   select: '#selectElement',
+//   events: {
+//     afterChange: newVal => {
+//       console.log(newVal);
+//     },
+//   },
 // });
 
 // npm install axios
 // npm install slim-select
 // npm i notiflix
 
-const BASE_URL = 'https://api.thecatapi.com/v1/';
-
-const instance = axios.create();
-axios.defaults.baseURL = BASE_URL;
-
-axios.defaults.headers.common['x-api-key'] =
-  'live_q0f7O1zpFYCLwEtRAEPlPq41gEIqmJ7E4HqeSMe2H2eQkgmN85txELlTKQq8z24d';
-
 const catList = document.querySelector('.breed-select');
 const elLoader = document.querySelector('.loader');
 const elError = document.querySelector('.error');
 const elInfo = document.querySelector('.cat-info');
 
-//elError.style.display = 'none';
-//catList.style.display = 'none;'
-
-fetchBreeds()
+fetchBreeds(catList, elLoader)
   .then(cats => renderCatList(cats))
   //.then(cats => console.log(cats))
   .catch(error => console.log(error));
-
-function fetchBreeds() {
-  return axios
-    .get(`breeds`)
-    .then(function (response) {
-      // if (!response.ok) {
-      //   throw new Error(response.status);
-      // }
-      //console.log(response.data);
-      catList.style.display = 'inline';
-      return response.data;
-    })
-    .catch(function (error) {
-      elLoader.style.display = 'none';
-      //elError.style.display = 'inline';
-      Notiflix.Notify.failure(
-        'Oops! Something went wrong! Try reloading the page!'
-      );
-    });
-}
-
-// function fetchBreeds() {
-//   return fetch(`${BASE_URL}breeds`).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   });
-// }
 
 function renderCatList(cats) {
   const markup = cats
@@ -69,46 +34,70 @@ function renderCatList(cats) {
         `;
     })
     .join('');
-  //console.log(markup);
   catList.innerHTML = markup;
   elLoader.style.display = 'none';
   catList.style.display.remove;
 }
 
-function fetchCatByBreed(id) {
-  return axios
-    .get(`images/search?breed_ids=${id}`)
-    .then(function (response) {
-      //console.log(response.data);
-      elInfo.style.display = 'inner';
-      return response.data;
-    })
-    .catch(function (error) {
-      //elError.textContent = error.response.data;
-      elLoader.style.display = 'none';
-      //elError.style.display = 'inline';
-      Notiflix.Notify.failure(
-        'Oops! Something went wrong! Try reloading the page!'
-      );
-    });
-}
-
 catList.addEventListener('change', () => {
   elError.style.display = 'none';
-  elLoader.style.display = 'inner';
+  elLoader.style.display = 'block';
   elInfo.style.display = 'none';
-  fetchCatByBreed(catList.options[catList.selectedIndex].value).then(catInfo =>
-    visibleCatInfo(catInfo)
-  );
+  fetchCatByBreed(
+    catList.options[catList.selectedIndex].value,
+    catList,
+    elLoader
+  ).then(catInfo => visibleCatInfo(catInfo));
 });
 
 function visibleCatInfo(catInfo) {
-  // console.log(catInfo[0].url);
-  // console.log(catInfo[0].breeds[0].description);
-  // console.log(catInfo[0].breeds[0].temperament);
   if (document.querySelector('img')) document.querySelector('img').remove();
   const catInfoHtml = `<img src="${catInfo[0].url}" width="320" />
                       <div><h1>${catInfo[0].breeds[0].name}</h1></div> <p>${catInfo[0].breeds[0].description}</p> 
                       <p><b>Temperament: </b>${catInfo[0].breeds[0].temperament}</p>`;
   elInfo.innerHTML = catInfoHtml;
 }
+
+// // Відправлення запросу на отримання ПОРІД котів
+// function fetchBreeds() {
+//   return axios
+//     .get(`breeds`)
+//     .then(function (response) {
+//       catList.style.display = 'inline';
+//       return response.data;
+//     })
+//     .catch(function (error) {
+//       elLoader.style.display = 'none';
+//       Notiflix.Notify.failure(
+//         'Oops! Something went wrong! Try reloading the page!'
+//       );
+//     });
+// }
+
+// // function fetchBreeds() {
+// //   return fetch(`${BASE_URL}breeds`).then(response => {
+// //     if (!response.ok) {
+// //       throw new Error(response.status);
+// //     }
+// //     return response.json();
+// //   });
+// // }
+
+// // Очікує ідентифікатор породи, робить HTTP-запит і повертає проміс із даними про кота
+// function fetchCatByBreed(id) {
+//   return axios
+//     .get(`images/search?breed_ids=${id}`)
+//     .then(function (response) {
+//       //console.log(response.data);
+//       elInfo.style.display = 'block';
+//       elLoader.style.display = 'none';
+//       return response.data;
+//     })
+//     .catch(function (error) {
+//       //elError.textContent = error.response.data;
+//       elLoader.style.display = 'none';
+//       Notiflix.Notify.failure(
+//         'Oops! Something went wrong! Try reloading the page!'
+//       );
+//     });
+// }
